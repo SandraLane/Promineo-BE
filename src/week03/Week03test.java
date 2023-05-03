375238f65b79e0b5f304baf461efb3caaab5a2d3
Sandra Lane <sandy6va@gmail.com>
2023-05-03 18:47:36 -0400

    first commit

---
+++ src/week03/Week03test.java
@@ -0,0 +1,13 @@
+package week03;
+
+public class Week03test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double value1 = 14.99;
+		double value2 = 15.99;
+		System.out.println(value1 + value2);
+
+	}
+
+}
